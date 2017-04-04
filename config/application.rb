66ec60953de202e61584393ba72fdeb1cd651b59
nagano564 <nagano564@gmail.com>
2017-04-04 14:57:23 -0400

    ?

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,5 @@ module Bloccit
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.autoload_paths << File.join(config.root, "lib")
-    config.assets.initialize_on_precompile = false
   end
 end
