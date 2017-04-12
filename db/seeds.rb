910a3d5151e21eb119274244078e1a20baa649c6
nagano564 <nagano564@gmail.com>
2017-04-11 21:10:04 -0400

    ckpt27 done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,14 @@
 require 'random_data'
+5.times do
+  User.create!(
+
+  name: RandomData.random_name,
+  email: RandomData.random_email,
+  password: RandomData.random_sentence
+  )
+end
+users = User.all
+
 # Create Topics
 15.times do
   Topic.create!(
@@ -11,6 +21,7 @@ topics = Topic.all
  50.times do
 
    Post.create!(
+    user:   users.sample,
     topic:  topics.sample,
     title:  RandomData.random_sentence,
     body:   RandomData.random_paragraph
@@ -31,7 +42,14 @@ topics = Topic.all
  Post.find_or_create_by(title: "A unique title", body: "A unique body")
  puts "#{Post.count}"
 
+ user = User.first
+ user.update_attributes!(
+   email: 'nagano564@gmail.com',
+   password: 'helloworld'
+ )
+
  puts "Seed finished"
+ puts "#{User.count} users created"
  puts "#{Topic.count} topics created"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
