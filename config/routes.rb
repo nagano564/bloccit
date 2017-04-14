db7939198de3bb255834aacfd349148f2dad19ba
nagano564 <nagano564@gmail.com>
2017-04-14 12:06:04 -0400

    ckpt29 done

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   resources :topics do
-# #34
     resources :posts, except: [:index]
+  end
+
+  resources :posts, only: [] do
+# #5
+    resources :comments, only: [:create, :destroy]
   end
 
   resources :users, only: [:new, :create]
